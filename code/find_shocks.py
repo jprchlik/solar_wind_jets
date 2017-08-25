b44cbbefd7e84316a78a0ba0063b55ea8e23f138
jprchlik <japrchlik@gmail.com>
2017-08-25 17:24:45 -0400

    Initial training set of shock values

--- code/find_shocks.py
+++ code/find_shocks.py
@@ -1,9 +1,18 @@
+import matplotlib as mpl
+mpl.use('TkAgg',warn=False,force=True)
+mpl.rcParams['lines.linewidth'] = 3
+mpl.rcParams['font.weight'] = 'bold'
+#mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.sans-serif'] = 'Helvetica'
+mpl.rcParams['font.size'] = 24
+
 import pandas as pd 
 import numpy as np
 from fancy_plot import fancy_plot
 from glob import glob
 import matplotlib.pyplot as plt
 from datetime import datetime,timedelta
+import statsmodels.api as sm
 
 
 
@@ -33,7 +42,7 @@ soho_df['shock'] = 0
 #locate shocks and update parameter to 1
 for i in shock_times.start_time_dt:
     #less than 120s seconds away from shock claim as part of shock (output in nano seconds by default)
-    shock, = np.where(np.abs(((soho_df.time_dt-i).values/1.e9).astype('float')) < 120.)
+    shock, = np.where(np.abs(((soho_df.time_dt-i).values/1.e9).astype('float')) < 70.)
     print soho_df['time_dt'][shock]
     soho_df['shock'][shock] = 1
 
@@ -42,36 +51,49 @@ for i in shock_times.start_time_dt:
 
 #calculate difference in parameters
 soho_df['del_time'] = soho_df['time_dt'].diff(-1).values.astype('double')/1.e9
-soho_df['del_speed'] = soho_df['SPEED'].diff(-1)/soho_df.del_time
-soho_df['del_Np'] = soho_df['Np'].diff(-1)/soho_df.del_time
-soho_df['del_Vth'] = soho_df['Vth'].diff(-1)/soho_df.del_time
+soho_df['del_speed'] = np.abs(soho_df['SPEED'].diff(-1)/soho_df.del_time)
+soho_df['del_Np'] = np.abs(soho_df['Np'].diff(-1)/soho_df.del_time)
+soho_df['del_Vth'] = np.abs(soho_df['Vth'].diff(-1)/soho_df.del_time)
+#create an array of constants that hold a place for the intercept 
+soho_df['intercept'] = 1 
 
 
 #mask for training set
-train = ((soho_df.time_dt >= datetime(2016,06,5,0)) & (soho_df.time_dt <= datetime(2016,12,31,0)) & (soho_df.del_time < 60.))
+train = ((soho_df.time_dt >= datetime(2016,06,5,0)) & (soho_df.time_dt <= datetime(2016,12,31,0)) & (soho_df.del_time < 60.) & (soho_df.shock == 1))
+no_sh = ((soho_df.time_dt >= datetime(2016,06,14,12)) & (soho_df.time_dt <= datetime(2016,06,16,0)) & (soho_df.del_time < 60.) & (soho_df.shock == 0))
 
 #training set
-soho_df_train = soho_df[train]#plot range 
+soho_df_train = soho_df[((train) | (no_sh))]#plot range 
 
 
+use_cols = ['del_speed','intercept']
+
+logit = sm.Logit(soho_df_train['shock'],soho_df_train[use_cols])
+sh_rs = logit.fit()
+
+mono_df = pd.DataFrame()
+mono_df['mono_speed'] = np.linspace(0,5,100)
+mono_df['mono_np'] = np.linspace(0,0.3,100)
+mono_df['int'] = 1
+mono_df['p_shock'] = sh_rs.predict(mono_df[['mono_speed','int']])
 
 
 fig,ax = plt.subplots(ncols=3)
 
 #plot solar wind speed
 ax[0].scatter(soho_df_train.del_speed,soho_df_train.shock,color='black')
-ax[0].set_xlabel('$|\mathrm{V}|$ [km/s]')
+ax[0].plot(mono_df.mono_speed,mono_df.p_shock,color='red')
+ax[0].set_xlabel('$\delta |\mathrm{V}|$ [km/s]')
 fancy_plot(ax[0])
 
 #plot solar wind density
 ax[1].scatter(soho_df_train.del_Np,soho_df_train.shock,color='black')
-ax[1].set_xlabel('n$_\mathrm{p}$ [cm$^{-3}$]')
-ax[1].set_xscale('log')
+ax[1].set_xlabel('$\delta$n$_\mathrm{p}$ [cm$^{-3}$]')
 fancy_plot(ax[1])
 
 #Thermal Speed
 ax[2].scatter(soho_df_train.Vth,soho_df_train.shock,color='black')
-ax[2].set_xlabel('w$_\mathrm{p}$ [km/s]')
+ax[2].set_xlabel('$\delta$w$_\mathrm{p}$ [km/s]')
 fancy_plot(ax[2])
 
 plt.show()
