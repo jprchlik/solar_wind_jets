12782aca8085b53bd357f691e8aaa3ac9d922dd1
jprchlik <japrchlik@gmail.com>
2017-08-29 17:21:37 -0400

    Added sunspots to plot

--- code/bin_shock_time.py
+++ code/bin_shock_time.py
@@ -9,7 +9,14 @@ mpl.rcParams['font.size'] = 24
 import pandas as pd
 import matplotlib.pyplot as plt
 from fancy_plot import fancy_plot
+from datetime import datetime
 
+#read sunspot file
+sunsp_df = pd.read_csv('../sun_spot_number/SN_m_tot_V2.0.txt',delim_whitespace=True)
+sunsp_df['time_dt'] = pd.to_datetime(sunsp_df.YYYY.astype('str')+sunsp_df.MM.astype('str'),format='%Y%m')
+sunsp_df.set_index(sunsp_df['time_dt'],inplace=True)
+#sunsp_df = sunsp_df[(sunsp_df.index >= datetime(1996,1,1,0)) & (sunsp_df.index < datetime(2016,1,1,0))]
+sunsp_df = sunsp_df[(sunsp_df.index >= datetime(2000,1,1,0)) & (sunsp_df.index < datetime(2016,1,1,0))]
 
 #read in shock file
 shock_df = pd.read_csv('../soho/archive_shocks.csv',sep=';')
@@ -21,14 +28,22 @@ shock_df['counts'] = 1
 
 #Resample at one month using the mean
 df_bin = shock_df.resample('12M').sum()
+df_spb = sunsp_df.resample('12M').sum()
 
 
 
-fig, ax = plt.subplots(figsize=(8,8))
+fig, ax = plt.subplots(figsize=(16,8),ncols=2)
 
-ax.scatter(df_bin.index,df_bin.counts)
-ax.set_ylabel('Number of Shock Events')
-ax.set_xlabel('Year')
-fancy_plot(ax)
+ax[0].scatter(df_bin.index,df_bin.counts)
+ax[0].plot(df_spb.index,df_spb.SN*2.)
+ax[0].set_ylabel('Number of Shock Events')
+ax[0].set_xlabel('Year')
+fancy_plot(ax[0])
+ax[0].set_xlim([datetime(1998,1,1),datetime(2016,1,1)])
+
+ax[1].scatter(df_bin.counts,df_spb.SN)
+ax[1].set_xlabel('Number of Shock Events')
+ax[1].set_ylabel('Number of Sun Spots')
+fancy_plot(ax[1])
 
 plt.show()
