c464f61e0cdc3b837fc6659bc376655fabb4b543
jprchlik <japrchlik@gmail.com>
2017-08-29 16:54:54 -0400

    Added logic to skip bokeh processing and now search formatted directory for shock files since pre 2004 contains less lines in header

--- code/find_shocks.py
+++ code/find_shocks.py
@@ -16,35 +16,52 @@ import statsmodels.api as sm
 
 
 
+#Use my training days
+mytrain = True 
+#Use full soho mission files
+full_soho = True 
+
+#create bokeh
+create_bokeh = False
+
 #Training set shock times to give a value of 1 for logit fitting
-shock_times = pd.read_table('shock_times.txt')
-shock_times['start_time_dt'] = pd.to_datetime(shock_times.start_time)
+if mytrain:
+    #shock_times = pd.read_table('shock_times_restricted.txt')
+    shock_times = pd.read_table('shock_times.txt')
+    shock_times['start_time_dt'] = pd.to_datetime(shock_times.start_time)
+else:
+    #Use shock spotter shock times
+    shock_times = pd.read_table('shock_spotter_events.txt',delim_whitespace=True)
+    shock_times = shock_times[shock_times.P > .5]
+    shock_times['start_time_dt'] = pd.to_datetime(shock_times.YY.astype('str')+'/'+shock_times.MM.astype('str')+'/'+shock_times.DD.astype('str')+'T'+shock_times.HHMM.astype('str'),format='%Y/%b/%dT%H%M')
+    
 
-
-#final all soho files in 30second cadence directory
-f_full = glob('../soho/data/30sec_cad/2*txt')
-#read in all soho files in 30sec_cad directory
-df_full = (pd.read_table(f,skiprows=28,engine='python',delim_whitespace=True) for f in f_full)
-#create one large array with all soho information
-full_df = pd.concat(df_full,ignore_index=True)
-#only keep with values in the Time frame
-full_df = full_df[full_df['DOY:HH:MM:SS'] >  0]
-
+#read in full mission long soho information
+if full_soho:
+    #final all soho files in 30second cadence directory
+    f_full = glob('../soho/data/30sec_cad/formatted_txt/2*txt')
+    #read in all soho files in 30sec_cad directory
+    df_full = (pd.read_table(f,engine='python',delim_whitespace=True) for f in f_full)
+    #create one large array with all soho information
+    full_df = pd.concat(df_full,ignore_index=True)
+    #only keep with values in the Time frame
+    full_df = full_df[full_df['DOY:HH:MM:SS'] >  0]
+    
+    
+    
+    
+    
+    #convert columns to datetime column
+    full_df['time_dt'] = pd.to_datetime(full_df['YY'].astype('int').map("{:02}".format)+':'+full_df['DOY:HH:MM:SS'],format='%y:%j:%H:%M:%S',errors='coerce')
+    #full_df['time_str'] = full_df['time_dt'].dt.strftime('%Y/%m/%dT%H:%M:%S')
+    #set index to be time
+    full_df.set_index(full_df['time_dt'],inplace=True)
 
 
 #find all soho files in data directory
 f_soho = glob('../soho/data/*txt')
-
 #read in all soho files in data directory
 df_file = (pd.read_table(f,skiprows=28,engine='python',delim_whitespace=True) for f in f_soho)
-
-#convert columns to datetime column
-full_df['time_dt'] = pd.to_datetime('20'+full_df['YY'].astype('int').map("{:02}".format)+':'+full_df['DOY:HH:MM:SS'],format='%Y:%j:%H:%M:%S',errors='coerce')
-#full_df['time_str'] = full_df['time_dt'].dt.strftime('%Y/%m/%dT%H:%M:%S')
-#set index to be time
-full_df.set_index(full_df['time_dt'],inplace=True)
-
-
 
 #create one large array with all soho information in range
 soho_df = pd.concat(df_file,ignore_index=True)
@@ -79,18 +96,19 @@ soho_df['intercept'] = 1
 
 #Do parameter calculation for all previous years 
 #calculate difference in parameters
-full_df['del_time'] = full_df['time_dt'].diff(-1).values.astype('double')/1.e9
-full_df['del_speed'] = np.abs(full_df['SPEED'].diff(-1)/full_df.del_time/full_df.SPEED)
-full_df['del_Np'] = np.abs(full_df['Np'].diff(-1)/full_df.del_time/full_df.Np)
-full_df['del_Vth'] = np.abs(full_df['Vth'].diff(-1)/full_df.del_time/full_df.Vth)
-#create an array of constants that hold a place for the intercept 
-full_df['intercept'] = 1 
+if full_soho:
+    full_df['del_time'] = full_df['time_dt'].diff(-1).values.astype('double')/1.e9
+    full_df['del_speed'] = np.abs(full_df['SPEED'].diff(-1)/full_df.del_time/full_df.SPEED)
+    full_df['del_Np'] = np.abs(full_df['Np'].diff(-1)/full_df.del_time/full_df.Np)
+    full_df['del_Vth'] = np.abs(full_df['Vth'].diff(-1)/full_df.del_time/full_df.Vth)
+    #create an array of constants that hold a place for the intercept 
+    full_df['intercept'] = 1 
 
 
 
 #mask for training set
-train = ((soho_df.time_dt >= datetime(2016,06,5,0)) & (soho_df.time_dt <= datetime(2016,12,31,0)) & (soho_df.del_time < 60.) & (soho_df.shock == 1))
-no_sh = ((soho_df.time_dt >= datetime(2016,06,14,12)) & (soho_df.time_dt <= datetime(2016,06,16,0)) & (soho_df.del_time < 60.) & (soho_df.shock == 0))
+train = ((soho_df.time_dt >= datetime(2016,6,5,0)) & (soho_df.time_dt <= datetime(2016,12,31,0)) & (soho_df.del_time < 60.) & (soho_df.shock == 1))
+no_sh = ((soho_df.time_dt >= datetime(2016,6,14,12)) & (soho_df.time_dt <= datetime(2016,6,16,0)) & (soho_df.del_time < 60.) & (soho_df.shock == 0))
 
 #training set
 soho_df_train = soho_df[((train) | (no_sh))]#plot range 
@@ -127,9 +145,10 @@ soho_df['predict'] = sh_rs.predict(soho_df[use_cols])
 #get predictions for training set
 soho_df_train['predict'] = sh_rs.predict(soho_df_train[use_cols])
 #get predictions for the Mission long CELIAS mission
-full_df['predict'] = sh_rs.predict(full_df[use_cols])
-best_df = full_df[full_df.predict >= 0.90]
-best_df.to_csv('../soho/archive_shocks.csv',sep=';')
+if full_soho: 
+    full_df['predict'] = sh_rs.predict(full_df[use_cols])
+    best_df = full_df[full_df.predict >= 0.90]
+    best_df.to_csv('../soho/archive_shocks.csv',sep=';')
 
 
 #create figure object
@@ -169,70 +188,70 @@ from bokeh.layouts import column,gridplot
 ##########################################
 #Create parameters for comparing data sets
 ##########################################
-source = ColumnDataSource(data=soho_df_train)
-tools = "pan,wheel_zoom,box_select,reset,hover,save,box_zoom"
-
-tool_tips = [("Date","@time_str"),
-             ("Del. Np","@del_Np"),
-             ("Del. Speed","@del_speed"),
-             ("Del. Vth","@del_Vth"),
-             ("Predict","@predict"),
-             ]
-
-
-p1 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
-#p1.plot_width = 1200
-p1.scatter('del_Np','shock',color='black',source=source)
-p1.select_one(HoverTool).tooltips = tool_tips
-p1.xaxis.axis_label = 'Delta Np/Np'
-p1.yaxis.axis_label = 'Shock'
-                                   
-p2 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
-#p2.plot_width = 1200
-p2.scatter('del_Vth','shock',color='black',source=source)
-p2.select_one(HoverTool).tooltips = tool_tips
-p2.xaxis.axis_label = 'Delta Vt/Vt'
-p2.yaxis.axis_label = 'Shock'
-                                   
-p3 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
-#p3.plot_width = 1200
-p3.scatter('del_speed','shock',color='black',source=source)
-p3.select_one(HoverTool).tooltips = tool_tips
-p3.xaxis.axis_label = 'Delta |V|/|V|'
-p3.yaxis.axis_label = 'Shock'
-                                   
-p4 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
-#p4.plot_width = 1200
-p4.scatter('time_dt','shock',color='black',source=source)
-p4.select_one(HoverTool).tooltips = tool_tips
-p4.xaxis.axis_label = 'UT Date'
-p4.yaxis.axis_label = 'Shock'
-                                   
-                                   
-save(gridplot([p1,p2],[p3,p4]),filename='bokeh_training_plot.html')
-
-###########################
-# Plotting for the full set
-###########################
-
-source1 = ColumnDataSource(data=soho_df)
-tools = "pan,wheel_zoom,box_select,reset,hover,save,box_zoom"
-
-tool_tips = [("Date","@time_str"),
-             ("Del. Np","@del_Np"),
-             ("Del. Speed","@del_speed"),
-             ("Del. Vth","@del_Vth"),
-             ("Predict","@predict"),
-             ]
-
-p5 = figure(title='SOHO CELIAS SHOCKS',tools=tools,x_axis_type="datetime")
-p5.plot_width = 1200
-p5.scatter('time_dt','predict',color='black',source=source1)
-p5.select_one(HoverTool).tooltips = tool_tips
-p5.xaxis.axis_label = 'UT Date'
-p5.yaxis.axis_label = 'Shock'
-                                   
-                                   
-save(p5,filename='bokeh_full_plot.html')
-
-
+if create_bokeh:
+    source = ColumnDataSource(data=soho_df_train)
+    tools = "pan,wheel_zoom,box_select,reset,hover,save,box_zoom"
+    
+    tool_tips = [("Date","@time_str"),
+                 ("Del. Np","@del_Np"),
+                 ("Del. Speed","@del_speed"),
+                 ("Del. Vth","@del_Vth"),
+                 ("Predict","@predict"),
+                 ]
+    
+    
+    p1 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
+    #p1.plot_width = 1200
+    p1.scatter('del_Np','shock',color='black',source=source)
+    p1.select_one(HoverTool).tooltips = tool_tips
+    p1.xaxis.axis_label = 'Delta Np/Np'
+    p1.yaxis.axis_label = 'Shock'
+                                       
+    p2 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
+    #p2.plot_width = 1200
+    p2.scatter('del_Vth','shock',color='black',source=source)
+    p2.select_one(HoverTool).tooltips = tool_tips
+    p2.xaxis.axis_label = 'Delta Vt/Vt'
+    p2.yaxis.axis_label = 'Shock'
+                                       
+    p3 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
+    #p3.plot_width = 1200
+    p3.scatter('del_speed','shock',color='black',source=source)
+    p3.select_one(HoverTool).tooltips = tool_tips
+    p3.xaxis.axis_label = 'Delta |V|/|V|'
+    p3.yaxis.axis_label = 'Shock'
+                                       
+    p4 = figure(title='SOHO CELIAS SHOCKS',tools=tools)
+    #p4.plot_width = 1200
+    p4.scatter('time_dt','shock',color='black',source=source)
+    p4.select_one(HoverTool).tooltips = tool_tips
+    p4.xaxis.axis_label = 'UT Date'
+    p4.yaxis.axis_label = 'Shock'
+                                       
+                                       
+    save(gridplot([p1,p2],[p3,p4]),filename='bokeh_training_plot.html')
+    
+    ###########################
+    # Plotting for the full set
+    ###########################
+    source1 = ColumnDataSource(data=soho_df)
+    tools = "pan,wheel_zoom,box_select,reset,hover,save,box_zoom"
+    
+    tool_tips = [("Date","@time_str"),
+                 ("Del. Np","@del_Np"),
+                 ("Del. Speed","@del_speed"),
+                 ("Del. Vth","@del_Vth"),
+                 ("Predict","@predict"),
+                 ]
+    
+    p5 = figure(title='SOHO CELIAS SHOCKS',tools=tools,x_axis_type="datetime")
+    p5.plot_width = 1200
+    p5.scatter('time_dt','predict',color='black',source=source1)
+    p5.select_one(HoverTool).tooltips = tool_tips
+    p5.xaxis.axis_label = 'UT Date'
+    p5.yaxis.axis_label = 'Shock'
+                                       
+                                       
+    save(p5,filename='bokeh_full_plot.html')
+    
+    
