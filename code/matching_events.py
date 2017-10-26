060f6e8e58e831f952d4f9422d8d3d108e0f28e0
jprchlik <japrchlik@gmail.com>
2017-10-26 17:10:55 -0400

    Added printing for matching events

--- code/matching_events.py
+++ code/matching_events.py
@@ -13,6 +13,7 @@ craft = ['wind','ace','dscovr','soho']
 #change craft order to change trainer 
 trainer = craft[0]
 
+
 #sigma level to use from wind training
 sig_l = 4.5
 #use sig_l to create shock prediction variable in dataframes
@@ -29,7 +30,35 @@ for k in craft: plsm[k] = pd.read_pickle('../{0}/data/y2016_power_formatted.pic'
 tr_events = plsm[trainer][plsm[trainer][p_var] > p_val]
 
 #get strings for times around each event#
-window = pd.to_timedelta('60 minutes')
+window = {}
+window['dscovr'] = pd.to_timedelta('40 minutes')
+window['ace'] = pd.to_timedelta('60 minutes')
+window['soho'] = pd.to_timedelta('60 minutes')
 
+#space craft to match with trainer soho
+craft.remove(trainer)
 
 #get event slices 
+for i in tr_events.index:
+    print('###############################')
+    print('NEW EVENT')
+    print(trainer)
+    print('{0:%Y/%m/%d %H:%M:%S}, p={1:4.3f}'.format(i,tr_events.loc[i,p_var]))
+    #get time slice around event
+
+    #loop over all other space craft
+    for k in craft:
+        time_slice = [i-window[k],i+window[k]] 
+        p_mat = plsm[k].loc[time_slice[0]:time_slice[1]] 
+
+
+        print(k)
+        if p_mat.size > 0:
+           #get the index of the maximum value
+           i_max = p_mat[p_var].idxmax() 
+           print('{0:5.2f} min., p_max={1:4.3f}'.format((i-i_max).total_seconds()/60.,p_mat.loc[i_max][p_var]))
+        else:
+           print('No Observations')
+    print('###############################')
+        
+    
