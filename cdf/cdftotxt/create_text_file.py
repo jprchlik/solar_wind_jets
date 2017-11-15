b470c5155c14bdcda4d7e82fc08d6c16a3e5315f
jprchlik <japrchlik@gmail.com>
2017-11-15 16:50:51 -0500

    Fixed Cadence error in decimated spacecraft B parameters

--- cdf/cdftotxt/create_text_file.py
+++ cdf/cdftotxt/create_text_file.py
@@ -26,7 +26,7 @@ def looper(s_idx):
         pls_key = ['Epoch','Proton_V_nonlin','Proton_Np_nonlin','Proton_W_nonlin','fit_flag']
     if sc1 == 'ace':
         mag_key = ['Epoch','BGSEc','Q_FLAG']
-        pls_key = ['Epoch','SPEED','Np','Vp','alpha_ratio']
+        pls_key = ['Epoch','SPEED','Np','Tpr','alpha_ratio']
     if sc1 == 'dscovr':
         mag_key = ['Epoch1','B1GSE','FLAG1']
         pls_key = ['Epoch','SPEED','Np','THERMAL_SPD','DQF']
@@ -37,11 +37,16 @@ def looper(s_idx):
     
     #convert to textfile
     cdf_to_text(fpls,pls_key,sc1,'pls')
-    cdf_to_text(fmag,mag_key,sc1,'mag')
+    #commented out J. Prchlik 2017/11/14 to fix wrong Vth in ACE
+    #cdf_to_text(fmag,mag_key,sc1,'mag')
 
 #function to create pandas dataframe
 def cdf_to_text(f_list,keys,craft,context):
 
+    #boltzman constant and proton mass for conversion of temp to speed
+    amu = 1.660538921e-27
+    mp  = 1.00727647*amu
+    kb  = 1.3906488e-23
 
     #oupt variable in header
     if context =='pls':
@@ -72,9 +77,13 @@ def cdf_to_text(f_list,keys,craft,context):
         #loop through cdf and write
         if ((context == 'pls') & (craft == 'wind')):
             for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(cdf[keys[4]][k]),float(cdf[keys[1]][k]),float(cdf[keys[2]][k]),float(cdf[keys[3]][k])))
-        elif ((context == 'pls') & (craft != 'wind')):
+        elif ((context == 'pls') & (craft == 'dscovr')):
             SPEED = np.sqrt(np.sum(cdf['V_GSE'][...]**2,axis=1))
             for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(cdf[keys[4]][k]),SPEED[k],float(cdf[keys[2]][k]),float(cdf[keys[3]][k])))
+        elif ((context == 'pls') & (craft == 'ace')):
+            SPEED = np.sqrt(np.sum(cdf['V_GSE'][...]**2,axis=1))
+            Vth   = 1.E-3*np.sqrt(2.*kb/mp*cdf[keys[3]][...]) #convert Thermal Temp to Speed
+            for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(cdf[keys[4]][k]),SPEED[k],float(cdf[keys[2]][k]),float(Vth[k])))
         elif ((context == 'mag') & (craft == 'wind')):
             #decrease the wind cadence 10 s in magfield
             for k,j in enumerate(cdf[keys[0]][...][::90]): out_fil.write(out_fmt.format(j[0],int(cdf[keys[2]][0]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
@@ -98,10 +107,12 @@ def cdf_to_text(f_list,keys,craft,context):
 #s_idx = 0
 
 #Do in parallel
-pool = Pool(processes=3)
-out  = pool.map(looper,range(3))
-pool.close()
-pool.join()
+#pool = Pool(processes=3)
+#out  = pool.map(looper,range(3))
+#pool.close()
+#pool.join()
 
+#just ace to fix wrong thermal speed
+looper(1)
 
 #for s_idx in range(3): looper(s_idx)
