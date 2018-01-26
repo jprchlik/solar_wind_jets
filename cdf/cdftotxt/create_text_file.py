932549c9d6d0d34d69d94644553562fbf8d75458
jprchlik <japrchlik@gmail.com>
2018-01-26 13:33:12 -0500

    Add python script which convert cdf files into text files

--- cdf/cdftotxt/create_text_file.py
+++ cdf/cdftotxt/create_text_file.py
@@ -11,18 +11,22 @@ def looper(s_idx):
     scrf = ['wind','ace','dscovr','soho']
     #current space craft
     sc1 = scrf[s_idx]
+
     
     
     #archive location of all spacecraft
-    archive='/Volumes/Pegasus/jprchlik/dscovr/solar_wind_events/cdf'#set output cdf location
+    archive='..'#set output cdf location
     archive = archive+'/'+sc1+'/plsm/'
-    mrchive='/Volumes/Pegasus/jprchlik/dscovr/solar_wind_events/cdf'#set output cdf location
+    mrchive='..'#set output cdf location
     mrchive = mrchive+'/'+sc1+'/mag/'
+    orchive='..'#set output cdf location
+    orchive = orchive+'/'+sc1+'/orb/'
+
     
     
     #setup keys for particular spacecraft
     if sc1 == 'wind':
-        mag_key = ['Epoch','BGSE','FLAG1_I']
+        mag_key = ['Epoch','BGSE','SPC_MODE']
         pls_key = ['Epoch','Proton_V_nonlin','Proton_Np_nonlin','Proton_W_nonlin','fit_flag']
     if sc1 == 'ace':
         mag_key = ['Epoch','BGSEc','Q_FLAG']
@@ -32,13 +36,15 @@ def looper(s_idx):
         pls_key = ['Epoch','SPEED','Np','THERMAL_SPD','DQF']
 
     #Get magnetic and plasma cdf files
-    fpls = glob(archive+'*cdf')
-    fmag = glob(mrchive+'*cdf')
+    fpls = glob(archive+'*20170[8-9]*cdf')
+    fmag = glob(mrchive+'*k1*cdf')
     
     #convert to textfile
-    cdf_to_text(fpls,pls_key,sc1,'pls')
+    #Commented to fix time error J. Prchlik 2017/11/14
+    #just ace currupted magnetic field observations
+    #cdf_to_text(fpls,pls_key,sc1,'pls')
     #commented out J. Prchlik 2017/11/14 to fix wrong Vth in ACE
-    #cdf_to_text(fmag,mag_key,sc1,'mag')
+    cdf_to_text(fmag,mag_key,sc1,'mag')
 
 #function to create pandas dataframe
 def cdf_to_text(f_list,keys,craft,context):
@@ -61,11 +67,13 @@ def cdf_to_text(f_list,keys,craft,context):
     out_fmt = '{0:%Y/%m/%dT%H:%M:%S}  {2:15.2f}  {3:15.2f}  {4:15.2f}             {1}\n'
 
     #output text file
-    out_fil = open('{0}_{1}_formatted.txt'.format(craft,context),'w')
+    out_fil = open('{0}_{1}_2017_2017_formatted.txt'.format(craft,context),'w')
 
 
     #write header
     out_fil.write(out_hdr.format(*header))
+
+   
 
     #loop over all files and write text file
     for i in f_list:
@@ -86,12 +94,16 @@ def cdf_to_text(f_list,keys,craft,context):
             for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(cdf[keys[4]][k]),SPEED[k],float(cdf[keys[2]][k]),float(Vth[k])))
         elif ((context == 'mag') & (craft == 'wind')):
             #decrease the wind cadence 10 s in magfield
-            for k,j in enumerate(cdf[keys[0]][...][::90]): out_fil.write(out_fmt.format(j[0],int(cdf[keys[2]][0]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
+            loopers = range(0,len(cdf[keys[0]][...]),90) 
+            for k in loopers: out_fil.write(out_fmt.format(cdf[keys[0]][k][0],int(cdf[keys[2]][k]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
         elif ((context == 'mag') & (craft == 'dscovr')):
             #decrease the wind cadence 10 s in magfield
-            for k,j in enumerate(cdf[keys[0]][...][::10]): out_fil.write(out_fmt.format(j,int(cdf[keys[2]][0]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
+            loopers = range(0,len(cdf[keys[0]][...]),10) 
+            for k in loopers: out_fil.write(out_fmt.format(cdf[keys[0]][k],int(cdf[keys[2]][k]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
         elif ((context == 'mag') & (craft == 'ace')):
-            for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(cdf[keys[2]][0]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
+            #for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(cdf[keys[2]][k]),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
+            #Hacked for k1 observations
+            for k,j in enumerate(cdf[keys[0]][...]): out_fil.write(out_fmt.format(j,int(0),(cdf[keys[1]][k][0]),cdf[keys[1]][k][1],cdf[keys[1]][k][2]))
 
 
 
@@ -105,14 +117,18 @@ def cdf_to_text(f_list,keys,craft,context):
 
 #loop over spacecraft
 #s_idx = 0
+#fix index error in DSCOVR and Wind Mag. Field
+#Run 2015-2017 observations
+ids = [0,1,2]
 
 #Do in parallel
 #pool = Pool(processes=3)
-#out  = pool.map(looper,range(3))
+#out  = pool.map(looper,ids)
 #pool.close()
 #pool.join()
 
 #just ace to fix wrong thermal speed
+#just ace currupted magnetic field observations
 looper(1)
 
 #for s_idx in range(3): looper(s_idx)
