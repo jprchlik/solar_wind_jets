d21094de07c03691080e3ca748041cf8826da8fe
jprchlik <japrchlik@gmail.com>
2018-01-26 12:12:56 -0500

    Added code which checks if file exists then downloads if not

--- cdf/get_cdf_files.py
+++ cdf/get_cdf_files.py
@@ -2,6 +2,7 @@ import ftplib
 import urllib
 from datetime import datetime,timedelta
 import itertools
+import sys,os
 
 
 def set_directory(craft,param):
@@ -33,10 +34,61 @@ def set_directory(craft,param):
     #return specific directory
     return a_dir
 
+#get list of years to look for data
+def return_years(start,end):
+    return range(start.year,end.year+1)
 
+#wrapper for download files program
+def download_files_wrapper(args):
+    return download_files(*args)
+
+#Actually where we download files
+def grab_files(craft,param,year,start,end,ftp):
+
+    #get subdirectory location on ftp server
+    a_dir = set_directory(craft,param)
+    #change ftp base directory
+    ftp.cwd(a_dir)
+
+    #list of files to download
+    d_list = []
+    #loop over all years and look for files
+    for i in year:
+        #get list of files in directory
+        f_list = ftp.nlst('{0:4d}/*cdf'.format(i))
+        #get just file names and check if they exist locally
+        for f_name in f_list:
+            #get local name for file
+            l_name = f_name.split('/')[-1] 
+
+            #output file
+            o_file = '{0}/{1}/{2}'.format(craft,param,l_name)
+
+            #check if local file exists
+            l_chck = os.path.isfile(o_file) == False
+            #if file does not exist or there is only 1 cdf in directory, download 
+            if ((l_chck) | (len(f_list) == 1)): 
+                fhandle = open(o_file,'wb')
+                ftp.retrbinary('RETR {0}'.format(f_name),fhandle.write)
+                fhandle.close()
+     
+    
+#download the archive files locally
+def download_files(craft,param,archive,years,start,end):
+
+    #ftp connection
+    ftp = ftplib.FTP(archive.split('/')[2],'anonymous')
+    try:
+        grab_files(a_dir,year,start,end,ftp)
+    except:
+        print('FTP connection failed unexpectly. Closing connection',sys.exc_info()[0])
+    #close ftp connection
+    ftp.close()
+
+#Main function to run
 def main(f_types=['mag','plsm','orb'],space_c=['ace','dscovr','wind'],
          archive='ftp://cdaweb.gsfc.nasa.gov/pub/data/',start=datetime(2015,6,1),end=None,
-         bdir='ftp://cdaweb.gsfc.nasa.gov/pub/data/'):
+         nproc=6):
 
 
     #if end time is not specified set now to end date
@@ -45,3 +97,22 @@ def main(f_types=['mag','plsm','orb'],space_c=['ace','dscovr','wind'],
 
     #get all combinations of parameters and spacecraft
     comb = itertools.product(space_c,f_types)
+
+    #year to search for data
+    years = return_years(start,end)
+
+
+    #create input list to loop over
+    input_list = []
+
+    #create directories and make input list
+    for i in comb:
+        #make subdirectories
+        try:
+            os.makedirs('{0}/{1}'.format(i[0],i[1]))
+        except OSError:
+            print('Directory Already Exists. Proceeding...')
+        input_list.append([i[0],i[1],archive,years,start,end])
+
+    #loop and download files
+    for i in input_list: download_files_wrapper(i)
