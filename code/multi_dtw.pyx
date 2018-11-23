f31a7d54a480a5ae700d01ed4782fe813a57c6a6
jprchlik <japrchlik@gmail.com>
2018-11-23 17:15:25 -0500

    Removed incorrect multi DTW functions added penalty parameter back in

--- code/multi_dtw.pyx
+++ code/multi_dtw.pyx
@@ -3,51 +3,6 @@ import numpy as np
 DTYPE = np.double
 uTYPE = np.long
 
-def dtw_grid(double [:,:] var_1, double [:,:] var_2,long lim_r, double pen_r):
-
-    #Define variables used for looping
-    cdef long var_1_x = var_1.shape[0]   
-    cdef long var_1_y = var_1.shape[1]   
-    cdef long var_2_x = var_2.shape[0]   
-    cdef long var_2_y = var_2.shape[1]   
-    cdef long x1,y1,y2
-    cdef double [:,:,:] diff_arr = np.zeros((var_1_x,var_1_y,var_2_y),dtype=DTYPE)
-    cdef double m,dy,penalty
-
-
-    #check to make sure x and y have the same number of variables (not the samp time series length)
-    if var_1_x != var_2_x:
-        raise ValueError("var_1 and var_2 must have the same number of variables. Currently, var_1=%d, var_2=%d" % [var_1_x,var_2_x]) 
-
-
-    #get the offset slope to allow for penalties when there is too much DTW compression
-    #assumes they have the same start time
-    m = var_1_y/var_2_y    
-
-    #loop over x variables
-    for x1 in range(var_1_x):
-        #loop over time variable for input number 1
-        for y1 in range(var_1_y):
-            #loop over time varaible for input number 2
-            for y2 in range(var_2_y):
-               
-                #add penalty
-                penalty = 0.
-                #check to see if time index is outside compression radius
-                d_y = abs(m*y2-y1)
-
-                #if d_y larger than the compression radius add a penalty
-                if d_y >= lim_r:
-                    penalty = pen_r
-
-                #near the start and end remove the penalty
-                if ((y1 <= lim_r) | (y2 <= lim_r) | (var_1_y-y1 <=lim_r) | (var_2_y-y2 <= lim_r)):  
-                    penalty = 0.
-                
-                #store the difference
-                diff_arr[x1,y1,y2] = abs(var_1[x1,y1]-var_2[x1,y2])+penalty
-
-    return diff_arr
 
 def dtw_single(double [:] var_1, double [:] var_2,long lim_1, long lim_2, double pen_r):
     """
@@ -127,7 +82,7 @@ def dtw_single(double [:] var_1, double [:] var_2,long lim_1, long lim_2, double
                 cumulative = min(back_cells)
             
             #store the difference
-            diff_arr[x1,x2] = abs(var_1[x1]-var_2[x2])+cumulative#**2+penalty#*d_x/lim_r
+            diff_arr[x1,x2] = abs(var_1[x1]-var_2[x2])+cumulative+penalty#*d_x/lim_r
 
     return diff_arr
 
@@ -298,70 +253,3 @@ def dtw_path_single(double [:] var_1, double [:] var_2,long lim_1, long lim_2, d
         return np.array(path_var_1[start_ind:]),np.array(path_var_2[start_ind:]),np.array(diff_arr)
     else:                          
         return np.array(path_var_1[start_ind:]),np.array(path_var_2[start_ind:])
-
-def dtw_path(double [:,:] var_1, double [:,:] var_2,long lim_r, double pen_r, long cost_matrix):
-
-    cdef long var_1_x = var_1.shape[0]   
-    cdef long var_1_y = var_1.shape[1]   
-    cdef long var_2_x = var_2.shape[0]   
-    cdef long var_2_y = var_2.shape[1]   
-    cdef long i=var_1_y-1,j=var_2_y-1,l=0
-    cdef long max_len = max([var_1_y,var_2_y])
-    cdef long min_len = max([var_1_y,var_2_y])
-    cdef double rat_len = float(max_len)/float(min_len)
-    cdef double[:,:,:] diff_arr = np.zeros((var_1_x,var_1_y,var_2_y),dtype=DTYPE)
-    cdef long [:] path_var_1 = np.zeros(max_len*int(rat_len+1.),dtype=uTYPE)-999
-    cdef long [:] path_var_2 = np.zeros(max_len*int(rat_len+1.),dtype=uTYPE)-999
-    cdef double i_cost,j_cost,b_cost
-
-    #check to make sure x and y have the same number of variables (not the samp time series length)
-    if var_1_x != var_2_x:
-        raise ValueError("var_1 and var_2 must have the same number of variables. Currently, var_1=%d, var_2=%d" % [var_1_x,var_2_x]) 
-
-    #get the difference array
-    diff_arr = dtw_grid(var_1,var_2,lim_r,pen_r)
-
-    #calculate the path
-    while ((i > 0) & (j > 0) & (l < max_len*round(rat_len+1.))) :
-        #i index already at zero then just decrease j index
-        if i == 0:
-            j = j -1 
-        #j index already at zero then just decrease i index
-        elif j == 0:
-            i = i -1 
-        else:
-            #calculate the cost for changing each element 
-            i_cost = sum(diff_arr[:,i-1,j])
-            j_cost = sum(diff_arr[:,i,j-1])
-            b_cost = sum(diff_arr[:,i-1,j-1])
-            #caclulate the min
-            m_cost = min([i_cost,j_cost,b_cost])
-
-            #update i,j index in path
-            if i_cost == m_cost:
-                i = i-1
-            elif j_cost == m_cost:
-                j = j-1
-            else:
-                j = j-1
-                i = i-1
-
-        #store the path variable
-        path_var_1[l] = i
-        path_var_2[l] = j
-
-        #update the path variable index 
-        l = l+1
-    #Add 0 0 to the last index
-    path_var_1[l] = 0
-    path_var_2[l] = 0
-
-    #flip the order of the output array
-    path_var_1 = np.array(path_var_1)[::-1]
-    path_var_2 = np.array(path_var_2)[::-1]
-
-    #remove all -999 values in return
-    if cost_matrix == 1:
-        return np.array(path_var_1[l+3:]),np.array(path_var_2[l+3:]),np.array(diff_arr)
-    else:                         
-        return np.array(path_var_1[l+3:]),np.array(path_var_2[l+3:])
